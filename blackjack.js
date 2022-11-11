3f90f7f44bf367ead33d7b0d57d26729314a3f26
Latte-47 <115649083+Latte-47@users.noreply.github.com>
2022-11-11 20:58:49 +0530

    A very basic Blackjack game completed.

--- blackjack.js
+++ blackjack.js
@@ -1,19 +1,39 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let player = {
+    name: "Latte",
+    chips: 145
+}
+
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
-    return 5
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    
+    if(randomNumber === 1) {
+        return 11
+    } else if(randomNumber >= 11) {
+        return 10
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    
     renderGame()
 }
 
@@ -38,10 +58,13 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard()
-    sum += card
-    cards.push(card)
-    console.log(cards)
-    renderGame()
+    if(isAlive === true && hasBlackjack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        console.log(cards)
+        renderGame()
+    }
+    
 }
 
